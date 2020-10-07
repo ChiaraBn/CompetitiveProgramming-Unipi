20c44add3f6094adb1d5f90a849d9acbc761b319
Chiara <chiara_boni@outlook.it>
2020-10-07 12:05:02 +0200

    exercise

---
+++ Lecture_03/monkey.cc
@@ -0,0 +1,56 @@
+/*
+ * Original problem at: 
+ * https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=3183
+ */
+
+#include <iostream>
+#include <vector>
+
+
+void findK (std:: vector<int> const &v, int num) {
+    int k = 0, diff = 0;
+
+    for (int i = v.size()-1; i >= 0; i--) {
+        if (i == 0) {
+            diff = v[i];
+        }
+        else {
+            diff = v[i] - v[i-1];
+        }
+
+        if (diff > k) {
+            k = diff;
+        }
+        else if (diff == k) {
+            k = diff + 1;
+        }
+    }
+    
+    std:: cout << "Case " << num << ": " << k << std:: endl;
+}
+
+
+int main (void) {
+    
+    std:: vector<int> ladder;
+    int t = 0, n = 0, elem = 0;
+
+    std:: cout << "test cases: ";
+    std:: cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        std:: cout << "# rungs: ";
+        std:: cin >> n;
+        ladder.reserve (n);
+
+        for (int j = 0; j < n; j++) {
+            std:: cin >> elem;
+            ladder.push_back(elem);
+        }
+
+        findK (ladder, i+1);
+        ladder.clear();
+    }
+
+    return 0;
+}
